5d045a37a17020abdc7c38b3f03f9db6c7a1fc3c
sibbi16 <70058380+sibbi16@users.noreply.github.com>
2022-05-15 22:25:51 +0500

    details page complete

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,25 +1,131 @@
 import react from "react";
 import {
-   View,
-   Text,
-   TouchableOpacity,
-   TextInput,
-   Platform,
-   StyleSheet,
-   StatusBar,
-   Alert,
- } from "react-native";
- import { LinearGradient } from "expo-linear-gradient";
- import Ionicons from "@expo/vector-icons/Ionicons";
-import AsyncStorage  from '@react-native-async-storage/async-storage';
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Platform,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  Image,
+  Alert,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const DetailScreen = ({route , navigation}) => {
-   console.log(route.params.product)
-   return ( 
-      <View>
-         <Text>This is detail screen</Text>
+const DetailScreen = ({ route, navigation }) => {
+  const product = route.params.product;
+  console.log(product);
+  return (
+    <SafeAreaView style={{ backgroundColor: "#fff" }}>
+      <View style={styles.header}>
+        <Ionicons
+          name="arrow-back-outline"
+          size={28}
+          color="#000"
+          onPress={() => navigation.goBack()}
+        ></Ionicons>
+        <Text style={{ fontSize: 20, fontWeight: "bold", paddingLeft: 10 }}>
+          Details
+        </Text>
       </View>
-    );
-}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: 280,
+          }}
+        >
+          <Image
+            source={product.item.image}
+            style={{ height: 220, width: 220, borderRadius: 100 }}
+          />
+        </View>
+        <View style={styles.details}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ fontSize: 25, color: "#fff", fontWeight: "bold" }}>
+              {product.item.name}
+            </Text>
+            <View style={styles.iconContainer}>
+              <Ionicons
+                name="heart-outline"
+                size={25}
+                color="#A17818"
+              ></Ionicons>
+            </View>
+          </View>
+          <Text style={styles.detailsText}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries.
+          </Text>
+          <View style={{ marginTop: 40, marginBottom: 40 }}>
+            <TouchableOpacity
+              style={{
+                width: "100%",
+                height: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#fff",
+                borderRadius: 10,
+              }}
+            >
+              <Text
+                style={{
+                  color: "#A17818",
+                  fontSize: 20,
+                }}
+              >
+                Add to cart
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
+const styles = StyleSheet.create({
+  header: {
+    paddingVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  details: {
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    paddingBottom: 60,
+    backgroundColor: "#A17818",
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+  },
+  iconContainer: {
+    backgroundColor: "#fff",
+    height: 50,
+    width: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+  },
+  detailsText: {
+    marginTop: 10,
+    lineHeight: 22,
+    fontSize: 16,
+    color: "#fff",
+  },
+});
 export default DetailScreen;
